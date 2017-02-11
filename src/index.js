97d1b6fd25372451fbcf41ebef909c5479fd74a7
Paolo Basso <paolo.basso87@gmail.com>
2017-02-11 11:05:44 +0100

    Automatically authenticating users

--- src/index.js
+++ src/index.js
@@ -5,23 +5,37 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 
+import { AUTH_USER } from './actions/types';
+
 import App from './components/app';
+import Welcome from './components/welcome';
 import SignIn from './components/auth/signin';
 import SignOut from './components/auth/signout';
 import SignUp from './components/auth/signup';
 import Feature from './components/feature';
 import reducers from './reducers';
 
+import requireAuth from './components/auth/requireAuth';
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+
+// If I have a token, the user is signed in
+if ( token ) {
+  // update the state to authenticated=true
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
+        <IndexRoute component={Welcome} />
         <Route path='signin' component={SignIn} />
         <Route path='signout' component={SignOut} />
         <Route path='signup' component={SignUp} />
-        <Route path='feature' component={Feature} />
+        <Route path='feature' component={requireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
